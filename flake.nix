544503a23e7d92824a21b015e6266945885edea4
functora <functora@proton.me>
2025-11-25 16:06:48 -0300

    init

---
+++ flake.nix
@@ -0,0 +1,54 @@
+{
+  description = "Rust Dev Shell";
+
+  inputs = {
+    unstable.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    unstable,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = unstable.legacyPackages.${system};
+        shell = {
+          packages = with pkgs; [
+            alejandra
+            bacon
+            cargo
+            cargo-edit
+            cargo-tarpaulin
+            clippy
+            rust-analyzer
+            rustc
+            rustfmt
+            license-generator
+          ];
+        };
+        mkRustPkg = pkg:
+          pkgs.rustPlatform.buildRustPackage {
+            name = pkg;
+            src = pkgs.nix-gitignore.gitignoreSource [] ./${pkg};
+            cargoLock.lockFile = ./${pkg}/Cargo.lock;
+          };
+      in {
+        devShells.default = pkgs.mkShell shell;
+        devShells.unfree = pkgs.mkShell (shell
+          // {
+            packages =
+              shell.packages
+              ++ [
+                pkgs.qutebrowser
+                pkgs.antigravity
+              ];
+          });
+        packages = rec {
+          site = mkRustPkg "site";
+          default = self.packages.${system}.site;
+        };
+      }
+    );
+}
